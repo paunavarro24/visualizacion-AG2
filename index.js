b849e2a28154142b9f41a318cc602c84328eed2e
paunavarro24 <paunavarroginer2412@gmail.com>
2022-04-26 21:59:36 +0200

    2 files modified

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import './style.css';
 
 import * as d3 from 'd3';
-//Gráfico de columnas. Mi 1º gráfico
 
 var visualization = d3plus.viz()
 
@@ -13,6 +12,38 @@ var visualization = d3plus.viz()
 {"year": 1992, "name":"alpha", "value": 20},])
 
 .type("bar")
+
+.id("name")
+
+.x("year")
+
+.y("value")
+
+.draw()
+
+
+
+var visualization3 = d3plus.viz()
+.container("#viz3")
+.data([
+  {"year": 1991, "name":"alpha", "value": 15},
+  {"year": 1992, "name":"alpha", "value": 34},
+  {"year": 1991, "name":"alpha2", "value": 17},
+  {"year": 1992, "name":"alpha2", "value": 65},
+  {"year": 1991, "name":"beta", "value": 10},
+  {"year": 1992, "name":"beta", "value": 10},
+  {"year": 1991, "name":"beta2", "value": 40},
+  {"year": 1992, "name":"beta2", "value": 38},
+  {"year": 1991, "name":"gamma", "value": 5},
+  {"year": 1992, "name":"gamma", "value": 10},
+  {"year": 1991, "name":"gamma2", "value": 20},
+  {"year": 1992, "name":"gamma2", "value": 34},
+  {"year": 1991, "name":"delta", "value": 50},
+  {"year": 1992, "name":"delta", "value": 43},
+  {"year": 1991, "name":"delta2", "value": 17},
+  {"year": 1992, "name":"delta2", "value": 35}
+])
+.type("box")
 
 .id("name")
 
